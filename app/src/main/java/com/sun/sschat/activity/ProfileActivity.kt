713409030581ee3908e6f10d943c96d3603b2e11
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-07-06 19:02:56 +0600

    Profile activity added successfully

--- app/src/main/java/com/sun/sschat/activity/ProfileActivity.kt
+++ app/src/main/java/com/sun/sschat/activity/ProfileActivity.kt
@@ -1,16 +1,92 @@
 package com.sun.sschat.activity
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.sun.sschat.MainActivity
 import com.sun.sschat.R
 import com.sun.sschat.databinding.ActivityProfileBinding
+import com.sun.sschat.model.UserModel
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var auth : FirebaseAuth
+    private lateinit var database : FirebaseDatabase
+    private lateinit var storage : FirebaseStorage
+    private lateinit var selectedImg : Uri
+    private lateinit var dialog : AlertDialog.Builder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        dialog = AlertDialog.Builder(this)
+            .setMessage("Updating Profile...")
+            .setCancelable(false)
+
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        binding.userImage.setOnClickListener {
+             val intent = Intent()
+             intent.action = Intent.ACTION_GET_CONTENT
+             intent.type = "image/*"
+             startActivityForResult(intent, 1)
+        }
+
+        binding.continueBtn.setOnClickListener {
+            if(binding.userName.text!!.isEmpty()){
+                Toast.makeText(this, "Please enter your Name", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                uploadData()
+            }
+        }
+
+    }
+
+    private fun uploadData() {
+        val reference = storage.reference.child("Profile").child(Date().time.toString())
+        reference.putFile(selectedImg).addOnCompleteListener{
+            if(it.isSuccessful){
+                reference.downloadUrl.addOnSuccessListener { task->
+                    uploadInfo(task.toString())
+                }
+            }
+        }
+    }
+
+    private  fun uploadInfo(imgUrl: String){
+        val user = UserModel(auth.uid.toString(), binding.userName.text.toString(), auth.currentUser!!.phoneNumber.toString(), imgUrl)
+
+        database.reference.child("users")
+            .child(auth.uid.toString())
+            .setValue(user)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Data inserted", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(data != null){
+            if(data.data!=null){
+                selectedImg = data.data!!
+                binding.userImage.setImageURI(selectedImg)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/sun/sschat/model/UserModel.kt
@@ -0,0 +1,8 @@
+package com.sun.sschat.model
+
+data class UserModel(
+    val uid : String,
+    val name : String,
+    val number : String,
+    val imageUrl : String
+)
